cbf0a277192b8ad5593ba0240eaa7fb3f5b4b06e
liuzhuang <1439163149@qq.com>
2026-03-21 14:19:23 +0800

    Initial Commit

---
+++ multi-codex-limit-viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multi-codex-limit-viewer
+//
+//  Created by liuzhuangm4 on 2026/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multi-codex-limit-viewer/multi_codex_limit_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_codex_limit_viewerApp.swift
+//  multi-codex-limit-viewer
+//
+//  Created by liuzhuangm4 on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct multi_codex_limit_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
